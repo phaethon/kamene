dc47784a344ae9acccd0a390b65393e05c8ce2e4
Eriks Dobelis <phaethon@users.noreply.github.com>
2015-02-06 15:54:49 +0200

    Changed platform message

--- scapy/arch/__init__.py
+++ scapy/arch/__init__.py
@@ -55,7 +55,7 @@ WINDOWS=sys.platform.startswith("win32")
 X86_64 = not WINDOWS and (os.uname()[4] == 'x86_64')
 
 if not LINUX:
-	log_loading.error("scapy3k currently works only on Linux platform. More to come soon.")
+	log_loading.error("scapy3k currently works only on Linux platform. Create an issue for your platform request at https://github.com/phaethon/scapy")
 if not LINUX and not FREEBSD:
 	exit()
 
